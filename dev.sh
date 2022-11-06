7700167450446f898f22595a88939d0dc95c594f
Raymond <20248577+javaarchive@users.noreply.github.com>
2022-11-06 05:55:30 +0000

    dev script

---
+++ dev.sh
@@ -0,0 +1,2 @@
+# when bundled with the new parcel and a subject is not selected, problems fail to load. It appears that this bug is not present when serving the unbundled version. 
+python -m http.server 8080
